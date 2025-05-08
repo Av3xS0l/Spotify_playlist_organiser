2fa8923b63352e2eb65a61c98950f65c3c50219f
Av3xS0l <pgustavsr@gmail.com>
2025-05-09 00:13:41 +0300

    Some bugs in display. Will fix

--- ui/Window.py
+++ ui/Window.py
@@ -5,7 +5,8 @@ Shows the previous commands and lets user access the
 '''
 
 import os
-from datatypes import *
+import datatypes as dt
+import time
 
 # All of the hardcoded strings for ease of use
 
@@ -15,6 +16,8 @@ COLOR : int= 255
 SET_CLEAR = "\033[0m"
 def SET_FG_COL(x): return f"\033[38;5{x}m"
 def SET_BG_COL(x): return f"\033[48;5{x}m"
+
+
 
 class Widget:
     '''
@@ -29,10 +32,51 @@ class Widget:
         self.height = height
     
     def frame(self, buff: list[str]) -> str:
-        buff[self.y] = buff[self.y][:self.x]+SET_FG_COL(COLOR)+'╭' + '─'*(self.width-2) + '╮' + SET_CLEAR+buff[self.y][self.x+self.width:]
+        buff[self.y] = buff[self.y][:self.x] + \
+            SET_FG_COL(COLOR) + '╭' + '─'*(self.width-2) + '╮' + SET_CLEAR + \
+            buff[self.y][self.x+self.width:]
+    
         for i in range(self.y+1, self.y+self.height-1):
-            buff[i] = buff[i][:self.x]+SET_FG_COL(COLOR)+'│' + buff[i][self.x:self.x+self.width-2] +'│' + SET_CLEAR+buff[i][self.x+self.width:]
-        buff[self.y+self.height-1] = buff[self.y+self.height-1][:self.x]+SET_FG_COL(COLOR)+'╰' + '─'*(self.width-2) + '╯' + SET_CLEAR+buff[self.y+self.height-1][self.x+self.width:]
+            buff[i] = buff[i][:self.x]+SET_FG_COL(COLOR) + \
+                '│' + buff[i][self.x:self.x+self.width-2] +'│' + SET_CLEAR + \
+                buff[i][self.x+self.width:]
+        
+        buff[self.y+self.height-1] = buff[self.y+self.height-1][:self.x] + \
+            SET_FG_COL(COLOR)+'╰' + '─'*(self.width-2) + '╯' + SET_CLEAR + \
+            buff[self.y+self.height-1][self.x+self.width:]
+
+class Commands(Widget):
+    def __init__(self, name: str, x: int, y: int, width: int, height: int, maxSize: int = 5) -> None:
+        super().__init__(name, x, y, width, height)
+        self.commands = dt.Queue()
+        self.maxSize = maxSize
+    
+    def addCommand(self, command: str) -> None:
+        self.commands.enqueue(command)
+        if self.commands.size > self.maxSize:
+            self.commands.dequeue()
+
+    def frame(self, buff: list[str]) -> str:
+        buff[self.y] = buff[self.y][:self.x] + \
+            SET_FG_COL(COLOR) + '╭' + '─'*(self.width-2) + '╮' + SET_CLEAR + \
+            buff[self.y][self.x+self.width:]
+        
+        # print the commands
+        coms = []
+        for x in self.commands.peek():
+           coms.append(x) 
+    
+        for i in range(self.y+1, self.y+self.height-1):
+            text = coms[(i-self.y-1)] if (i-self.y-1) < len(coms) else ' '
+            buff[i] = buff[i][:self.x] + ' '*(self.width-2) + buff[i][self.x+self.width:]
+            middle = text+buff[i][self.x+len(text):self.x+self.width-2]
+            buff[i] = buff[i][:self.x]+SET_FG_COL(COLOR) + \
+                '│' + middle +'│' + SET_CLEAR + \
+                buff[i][self.x+self.width:]
+        
+        buff[self.y+self.height-1] = buff[self.y+self.height-1][:self.x] + \
+            SET_FG_COL(COLOR)+'╰' + '─'*(self.width-2) + '╯' + SET_CLEAR + \
+            buff[self.y+self.height-1][self.x+self.width:]
 
 
 class Window:
@@ -42,21 +86,38 @@ class Window:
     
     def __init__(self) -> None:
         self.width, self.height = os.get_terminal_size()
-        self.cursor = Point(0, 0)
+        self.cursor = dt.Point(0, 0)
         self.buffer: list[str] = [" "*self.width]*self.height
         self.offBuffer: list[str] = []
         self.objMap = {}
        
-       # widget initialization goes here
-        self.objMap['main'] = Widget('main', 0, 0, self.width, self.height)
+        # widget initialization goes here
+        self.objMap['main'] = Widget('main', 0, 0, self.width, self.height-5)
         self.objMap['main'].frame(self.buffer)
+
+        # widget for outputing recent calls
+        self.objMap['output'] = Commands('output', 0, self.height-5, self.width, 3)
+
+        self.objMap['output'].addCommand('test')
+        self.objMap['output'].addCommand('test2')
+        self.objMap['output'].addCommand('test3')
+        
+        self.objMap['output'].frame(self.buffer)
+
+
     
     def draw(self):
         for x in self.buffer:
             print(x)
 
     
-
+# testing
 
 win = Window()
+os.system('clear')
 win.draw()
+for i in range(5):
+    time.sleep(1)
+    win.objMap['output'].addCommand('test_'+str(i))
+    win.objMap['output'].frame(win.buffer)
+    win.draw()
