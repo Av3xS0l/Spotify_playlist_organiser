c7d98768858c3fce9642854ba8f405ca7fcbbd85
Av3xS0l <pgustavsr@gmail.com>
2025-05-19 16:54:46 +0300

    now saving data

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from api import Api
 from api import SongData
 import os
 from time import sleep
+import pickle
 
 
 def main() -> None:
-    
+    # Set up environment
     win = Window()
     R_CMD_H: int = 5
     SLEEP_SEC: int = 5
-    HEIGHT_MIN: int = 20
+    HEIGHT_MIN: int = 25
 
     SKIP_CNT_TRESHOLD: int = 3
     SKIP_TRESHOLD = 0.4
@@ -21,18 +22,13 @@ def main() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')    # clear the screen
 
     print('\033[?25l')  # hide the cursor
+
     if (win.height < HEIGHT_MIN):
         # Tereminal size too small
-        print('\033[31mTermināļa izmērs ir mazāks par 20 rindām\nNav iespējams atēlot visus elementus\nLūdzu palieliniet loga izmērus')
+        print(f'\033[31mTermināļa izmērs ir mazāks par {HEIGHT_MIN} rindām\nNav iespējams atēlot visus elementus\nLūdzu palieliniet loga izmērus')
         os.system('pause')
         print('\033[0m')
         return
-    elif (win.height <= 25):
-        # Scaling will be strange
-        print('\033[31mTermināļa izmērs nav lielāks par 25 rindām\nProgramma saturēs vizuālus artefaktus')
-        os.system('pause')
-        print('\033[0m')
-        os.system('cls' if os.name == 'nt' else 'clear')
 
     # Init of the widgets on the screen 
     win.add('main', 'Widget', 0, 1, win.width, win.height-R_CMD_H-1)
@@ -42,20 +38,27 @@ def main() -> None:
     # API Init
     api = Api()
 
+    # Draw the objects on the screen
     def drawLoop(image: str):
-
         win.objMap['cover'].convert(image)
         win.draw()
 
+
+
     prev_song = SongData()
 
-    SkippedSongs: dict[str, int] = {}
-
+    try:
+        with open('skipped_songs.pkl', 'rb') as f:
+            SkippedSongs: dict[str, int] = pickle.load(f)
+    except :
+        SkippedSongs: dict[str, int] = {}
 
     # Main loop
     while True:
         # get current song
         current_song = api.api_call()
+
+        # if no song is playing
         if current_song == None:
             win.objMap['commands'].addCommand("No song is currently playing", win.offBuffer, 'red')
             drawLoop(image=None)    
@@ -78,18 +81,25 @@ def main() -> None:
 
             # has the song been skipped early?
             if prev_song.song_id != '' and not prev_song.progress(SKIP_TRESHOLD):
+
                 win.objMap['commands'].addCommand(f"{prev_song.song_name} was skipped early", win.offBuffer, 'yellow')
+
+                # adding song to the skipped ones and removing if needed
                 if prev_song.song_id in SkippedSongs.keys():
                     SkippedSongs[prev_song.song_id] += 1
                     if SkippedSongs[prev_song.song_id] >= SKIP_CNT_TRESHOLD:
+                        # remove song from the playlist
                         api.remove_song(prev_song)
                         SkippedSongs.pop(prev_song.song_id)
                         win.objMap['commands'].addCommand(f"Removed {prev_song.song_name} from playlist", win.offBuffer, 'red')
                 else:
                     SkippedSongs.update({prev_song.song_id: 1})
-            
 
-            
+                # save state to file
+                with open('skipped_songs.pkl', 'wb') as f:
+                    pickle.dump(SkippedSongs, f)
+                    
+        # update the song and screen
         prev_song = current_song
         drawLoop(image=current_song.song_image)    
         sleep(SLEEP_SEC)
